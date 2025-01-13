dcf05f42f6423575ff2ccb94e148aff7751945ec
Elisei <eliseivlas.002@gmail.com>
2025-01-13 16:15:11 +0100

    creazione calcolo del prezzo

--- script/script.js
+++ script/script.js
@@ -13,11 +13,38 @@ const outPrezzo = document.getElementById('prezzo');
 
 // tramite l'invio del form prendiamo i valori dei campi selezionati e li scriviamo nei blocchi
 form.addEventListener('submit', (event) => {
+
     // Blocco l'invio del form..
     event.preventDefault();
 
-    // Tutto il resto del codice...
-    console.log("il form cerca di essere inviato");
+    //ELABORAZIONE
+    // calcolo il prezzo del percorso in base al killometraggio
+    let prezzoBiglietto = (killometriDaPercorrere*prezzoPerKillometro)
+    
+        //calcolo da fare nel caso eta inferiore a 18 anni
+        let scontoInferiore = (prezzoBiglietto*80/100);
+        
+        //calcolo da fare nel caso eta superiore a 64 anni
+        let scontoSuperiore =(prezzoBiglietto*60/100)
+
+     // calcolo lo sconto in base all'eta del passegero
+        //SE l'eta del passeggero e inferiore a 18 anni ha uno sconto del 20%
+        if (etaPas < 18) {
+            //sconto del 20%
+            prezzo = scontoInferiore.toFixed(2)
+            console.log("Il prezzo da pagare scontato del 20% e" + " " + prezzo + "€")
+            //ALTRIMENTI SE l'eta del passeggero e superiore a 64 anni ha uno sconto del 40%
+        }else if(etaPas > 64) {
+            //sconto del 40%
+            prezzo = scontoSuperiore.toFixed(2)
+            console.log("Il prezzo da pagare scontato del 40% e" + " " + prezzo + "€")
+
+            //Altrimenti il prezzo rimane non scontato
+        }else {
+            prezzo = prezzoBiglietto.toFixed(2);
+            console.log("Il prezzo da pagare e" + " " + prezzo + "€")
+        }    
+
     // prendi i selettori di output e scrivi dentro i valori presi dagli input
     title.innerHTML = titleField.value.trim();
     name.innerHTML = nameField.value.trim();
@@ -25,6 +52,5 @@ form.addEventListener('submit', (event) => {
     role.innerHTML = roleField.value.trim();
     email.innerHTML = emailField.value.trim();
     phone.innerHTML = phoneField.value.trim();
-    // scrivo all'interno dell'attributo src del tag img
-    companyPic.src = picField.value;
+
 });
